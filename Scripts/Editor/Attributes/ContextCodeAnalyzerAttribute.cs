b2565b22e2b6488c4a48f91a36ba5e1e1ad63308
Erik Takoev <eriktakoev@gmail.com>
2025-06-12 15:55:15 +0300

    Refactor ContextCodeAnalyzerAttribute to include detailed context information with purpose, usage, parameters, returns, and notes

--- Scripts/Editor/Attributes/ContextCodeAnalyzerAttribute.cs
+++ Scripts/Editor/Attributes/ContextCodeAnalyzerAttribute.cs
@@ -1,18 +1,35 @@
 using System;
+using System.Text;
 
 namespace Expecto
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property)]
-    public class ContextCodeAnalyzerAttribute : Attribute
-    {
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property)]
+	public class ContextCodeAnalyzerAttribute : Attribute
+	{
 #if UNITY_EDITOR
-        public string Context { get; }
+		public string Context { get; private set; }
 #endif
-        public ContextCodeAnalyzerAttribute(string context)
-        {
+
+		public ContextCodeAnalyzerAttribute(string @purpose, string @usage, string @params = null, string @returns = null, string @notes = null)
+		{
 #if UNITY_EDITOR
-            Context = context;
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Purpose: {@purpose}");
+			sb.AppendLine($"Usage: {@usage}");
+			if (@params != null)
+			{
+				sb.AppendLine($"Params: {@params}");
+			}
+			if (@returns != null)
+			{
+				sb.AppendLine($"Returns: {@returns}");
+			}
+			if (@notes != null)
+			{
+				sb.AppendLine($"Notes: {@notes}");
+			}
+			Context = sb.ToString();
 #endif
-        }
-    }
+		}
+	}
 }
